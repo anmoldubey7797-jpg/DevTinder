b8fca0268df96b2e192a123165b5b1f42232c78f
Anmol Dubey <anmoldubey7797@gmail.com>
2026-02-05 23:33:11 +0530

    feat: implement user signup and retrieval endpoints with error handling

--- src/index.js
+++ src/index.js
@@ -1,21 +1,73 @@
-import express from "express";
+import express, { response } from "express";
 import mongoose from "mongoose";
 import {User} from "./models/user.models.js"
 import connectDB from "./config/database.js";
  const app=express();
+ app.use(express.json())
 
 
- app.post("/signup",(req,res)=>{
-    const user=new User({
-        firstName:"Kamal",
-        lastName:"Kumar",
-        email:"kamal@gmail.com",
-        password:"kamal@123"
-    })
-
-    user.save();
-    res.send("User added Successfully")
+ app.post("/signup",async(req,res)=>{
+    
+    try{
+    const user=new User(req.body)
+    await user.save();
+     return res.status(201).json({success:true,message:"User Added successfully",data:user})
+    }
+    catch(error){
+      return   res.status(404).send("Error has been found",error.message)
+    }
+   
  })
+
+ app.get("/user",async(req,res)=>{
+    const userEmail=req.body.email;
+    try{
+      const user=await User.find({email:userEmail})
+
+    
+      if(user.length===0){
+       return  res.status(404).json({message:"User Not found"})
+      }
+      else{
+        // res.send(user)
+      return res.status(200).json({success:true,data:user})
+      } 
+    }
+    catch(error){
+        res.status(404).send("Error hai bhai")
+    }
+ })
+
+ app.get("/form",async(req,res)=>{
+    try{
+       const users=await User.find({});
+       console.log(users)
+       res.status(200).json({success:true,message:"All clear",data:users})
+      
+    }
+    catch(error){
+        res.status(404).send("Something went wrong")
+    }
+ })
+ app.get("/form1",async(req,res)=>{
+    try{
+       const userEmail=req.body.email;
+       const users=await User.findOne({email:userEmail});
+       console.log(users)
+       if(!users){
+       res.status(404).json({success:false,message:"All not Clear",data:users})
+       } 
+       else{
+        res.status(200).json({success:true,message:"All clear Bro"})
+        
+       }
+    }
+    catch(error){
+        res.status(404).send("Something went wrong",error.message)
+    }
+ })
+
+ 
 
 connectDB()
 .then(()=>{
